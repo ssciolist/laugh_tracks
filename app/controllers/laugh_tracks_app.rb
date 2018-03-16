8d87a5ac5c781f8d4b4dd792ae917cf46ace5309
Megan Arellano <ssciolist@gmail.com>
2018-03-16 10:36:03 -0600

    index displays header, no comedians yet

--- app/controllers/laugh_tracks_app.rb
+++ app/controllers/laugh_tracks_app.rb
@@ -1,2 +1,9 @@
 class LaughTracksApp < Sinatra::Base
+  get "/comedians" do
+    erb :comedians,
+      :locals => {
+        :all_comedians => Comedian.all
+      }
+  end
+
 end
--- spec/features/comedians/index_spec.rb
+++ spec/features/comedians/index_spec.rb
@@ -2,6 +2,7 @@ describe "user visits /comedians" do
   it "displays comedian list with name and age" do
   Comedian.new(name: 'Maria Bamford', age: 47)
   Comedian.new(name: 'Tig Notaro', age: 40)
+  visit "/comedians"
 
 
   expect(page).to have_content("Maria Bamford")
